3158f37f4a84247168f8cc0100bd4d557926489f
raavr <rafal.r26@gmail.com>
2018-08-13 18:33:49 +0200

    refactor: replace absolute paths by relative

--- src/app/words/words.module.ts
+++ src/app/words/words.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { WordsRoutingModule } from './words-routing.module';
 import { WordsComponent } from './words.component';
-import { WordItemComponent } from 'app/words/word-item/word-item.component';
-import { KeyboardShortcutComponent } from 'app/words/keyboard-shortcut/keyboard-shortcut.component';
+import { WordItemComponent } from './word-item/word-item.component';
+import { KeyboardShortcutComponent } from './keyboard-shortcut/keyboard-shortcut.component';
 
 
 @NgModule({
